b24d55151e356dc6919a85de63331ce0690e6137
Devin Berchtold <berchto2@eceb-2022-38.ews.illinois.edu>
2016-03-07 18:33:25 -0600

    added register sv files

---
+++ de_register.sv
@@ -0,0 +1,18 @@
+module de_register
+(
+    input clk,
+    input load,
+    /* inputs */
+    input [width-1:0] in,//replace this with a bunch of signals
+	 /* outputs */
+    output logic [width-1:0] out//replace this with a bunch of signals
+);
+
+//make a bunch of these
+register #(.width(2)) registername
+(
+    .clk,
+    .load_sig_name,
+    .in_name,
+    .out_name
+);
