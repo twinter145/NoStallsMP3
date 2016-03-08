22e91bff10c3b297158f7dd68fd1cbf763f216a8
Devin Berchtold <berchto2@eceb-2022-19.ews.illinois.edu>
2016-03-08 15:56:59 -0600

    made mem_register, added control to types

--- datapath.sv
+++ datapath.sv
@@ -2,13 +2,26 @@ import lc3b_types::*;
 
 module datapath
 (
-	/* inputs */
 	input clk,
 	
-	/* outputs */
+	/* memory signals */
+	//inputs
+	input mem_resp,
+	input lc3b_word mem_rdata,
+	//outputs
+	output mem_read,
+	output mem_write,
+	output lc3b_mem_wmask mem_byte_enable,
+	output lc3b_word mem_address,
+	output lc3b_word mem_wdata
+	
+	/* other stuff */
+	//inputs
+	
+	//outputs
 );
 //internal signals
-
+lc3b_word wb_mux_out;
 
 //fetch
 
@@ -16,13 +29,42 @@ module datapath
 //decode
 
 
+
 //execute
 
 
 //memory
+mem_register mem_register 
+(
+	.clk
+	.load
+);
 
+memory memory
+(
+);
 
 //write back
+wb_register wb_regsiter
+(
+	.clk,
+	.load,
+);
 
+mux4 wb_mux
+(
+	.sel(wb_mux_sel),
+	.a(wb_address_out),
+	.b(wb_rdata_out),
+	.c(wb_next_instr_out),
+	.d(wb_alu_out),
+	.f(wb_mux_out)
+);
+
+gencc gencc
+(
+	.in(wb_mux_out),
+	.out(gencc_out)
+);
 
 endmodule : datapath
--- mem_register.sv
+++ mem_register.sv
@@ -3,16 +3,93 @@ module mem_register
     input clk,
     input load,
     /* inputs */
-    input [width-1:0] in,//replace this with a bunch of signals
+    input lc3b_word ex_address,
+	 input lc3b_word ex_next_instr,
+	 input lc3b_control ex_control_sig,
+	 input [2:0] ex_cc,
+	 input lc3b_word ex_alu_out in,
+	 input lc3b_word ex_ir in,
+	 input lc3b_reg ex_dest,
+	 input [1:0] ex_valid,
 	 /* outputs */
-    output logic [width-1:0] out//replace this with a bunch of signals
+    output lc3b_word mem_address,
+	 output lc3b_word mem_next_instr,
+	 output lc3b_control mem_control_sig,
+	 output [2:0] mem_cc,
+	 output lc3b_word mem_alu_out in,
+	 output lc3b_word mem_ir in,
+	 output lc3b_reg mem_dest,
+	 output mem_valid,
 );
 
-//make a bunch of these
-register #(.width(2)) registername
+//address
+register #(.width(16)) address
 (
     .clk,
-    .load_sig_name,
-    .in_name,
-    .out_name
+    .load,
+    .ex_address,
+    .mem_address
+);
+
+//next_instr
+register #(.width(16)) next_instr
+(
+    .clk,
+    .load,
+    .ex_next_instr,
+    .mem_next_instr
+);
+
+//control signal
+register #(.width(1)) control_signal
+(
+    .clk,
+    .load,
+    .ex_control_signal,
+    .mem_control_signal
+);
+
+//cc
+register #(.width(3)) cc
+(
+    .clk,
+    .load,
+    .ex_cc,
+    .mem_cc
+);
+
+//alu out
+register #(.width(16)) alu_out
+(
+    .clk,
+    .load,
+    .ex_alu_out,
+    .mem_alu_out
+);
+
+//ir
+register #(.width(16)) ir
+(
+    .clk,
+    .load,
+    .ex_ir,
+    .mem_ir
+);
+
+//dest
+register #(.width(3)) dest
+(
+    .clk,
+    .load,
+    .ex_dest,
+    .mem_dest
+);
+
+//valid
+register #(.width(1)) valid
+(
+    .clk,
+    .load,
+    .ex_valid,
+    .mem_valid
 );
