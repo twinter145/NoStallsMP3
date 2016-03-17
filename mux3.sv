27ea178fdce14ee1d55e36f08bb35d25f97203da
Jacque Crossett <crosstt2@eceb-2022-18.ews.illinois.edu>
2016-03-17 15:32:50 -0500

    fixed mux3 module

--- mux3.sv
+++ mux3.sv
@@ -1,24 +1,5 @@
-<<<<<<< HEAD
 import lc3b_types::*;
 
-module mux3 #(parameter width = 16)
-(
-	input lc3b_mux_sel sel,
-	input [width-1:0] a, b, c,
-	output logic [width-1:0] f
-);
-always_comb
-begin
-	if (sel == 2'b00)
-		f = a;
-	else if (sel == 2'b01)
-		f = b;
-	else if (sel == 2'b10)
-		f = c;
-	else	
-		f = c;
-end
-=======
 //creates 4 input mux, sets up output
 module mux3 #(parameter width = 16)
 (
@@ -35,5 +16,5 @@ case(sel)
 	2: f = c;
 	3: f = 0;
 endcase
->>>>>>> 903bc3f056857b84ea56221fa777eac06e01fe7f
+
 endmodule : mux3
