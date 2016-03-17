dc833d5dbe7cc34925b3cb89cb0c3c8d8243529d
Thomas Winter <tmwinte2@gelib-057-12.ews.illinois.edu>
2016-03-16 20:27:20 -0500

    types

--- lc3b_types.sv
+++ lc3b_types.sv
@@ -1,6 +1,5 @@
 package lc3b_types;
 
-typedef logic [127:0] lc3b_line;
 typedef logic [15:0] lc3b_word;
 typedef logic  [7:0] lc3b_byte;
 
@@ -16,6 +15,11 @@ typedef logic  [2:0] lc3b_nzp;
 typedef logic  [1:0] lc3b_mem_wmask;
 typedef logic [1:0] lc3b_mux_sel;
 typedef logic [2:0] lc3b_mux8_sel;
+
+typedef logic [127:0] lc3b_line;
+typedef logic [8:0] lc3b_c_tag;
+typedef logic [2:0] lc3b_c_index;
+typedef logic [2:0] lc3b_c_offset;
 
 typedef enum bit [3:0] {
     op_add  = 4'b0001,
